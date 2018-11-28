8d8f293a371d0832f0f5adf733cc0e49de7b947b
jultrocor <j.troncosocorrea@gmail.com>
2018-11-28 21:02:47 +0100

    Añadidos tests de API

--- test/server.test.js
+++ test/server.test.js
@@ -1,5 +1,10 @@
+var server = require("../server");
 var chai = require("chai");
+var chaiHttp = require("chai-http");
+var sinon = require("sinon");
 var expect = chai.expect;
+
+chai.use(chaiHttp);
 
 describe("Contacts API", () => {
   it("hello test", done => {
@@ -9,6 +14,68 @@ describe("Contacts API", () => {
     var res = x + y;
 
     expect(res).to.equal(8);
+
     done();
+  });
+
+  before(done => {
+    var contacts = [
+      { name: "julio", phone: 1234 },
+      { name: "alberto", phone: 1234 }
+    ];
+
+    var dbFindStub = sinon.stub(server.db, "find");
+    dbFindStub.yields(null, contacts);
+
+    done();
+  });
+
+  describe("GET /", () => {
+    it("should return HTML", done => {
+      chai
+        .request(server.app)
+        .get("/")
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res).to.be.html;
+
+          done();
+        });
+    });
+  });
+
+  describe("GET /contacts", () => {
+    it("should return all contacts in JSON", done => {
+      chai
+        .request(server.app)
+        .get("/api/v1/contacts")
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res).to.be.json;
+          expect(res.body).to.be.an("array");
+          expect(res.body).to.have.lengthOf(2);
+
+          done();
+        });
+    });
+  });
+
+  describe("POST /contacts", () => {
+    it("should create a new contact", done => {
+      var contact = { name: "pablo", phone: 1234 };
+      var dbMock = sinon.mock(server.db);
+      dbMock.expects("insert").withArgs(contact);
+
+      chai
+        .request(server.app)
+        .post("/api/v1/contacts")
+        .send(contact)
+        .end((err, res) => {
+          expect(res).to.have.status(201);
+          dbMock.verify();
+
+          done();
+        });
+    });
   });
 });
